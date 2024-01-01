e9c053f883151353e7c83fdd66199a4215f86736
AssilMabrouk <155380220+AssilMabrouk@users.noreply.github.com>
2024-01-01 17:25:24 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let b3oo = randint(0, 10)
+basic.showNumber(b3oo)
+if (false && false) {
+	
+} else if (false && false) {
+	
+} else if (false && false) {
+	
+} else {
+	
+}
+basic.forever(function () {
+    while (false) {
+        for (let index = 0; index < 4; index++) {
+        	
+        }
+    }
+})
